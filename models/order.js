2733a701bd04f718472ddbe8fbbb25fb9025b21f
valere mandel <valeremandel@renata-Macbook-Pro.local>
2025-10-09 21:13:15 -0300

    sushi

--- models/order.js
+++ models/order.js
@@ -1,8 +1,25 @@
 import mongoose from "mongoose";
-const { Schema } = mongoose;
 
-const orderSchema = new Schema({
- 
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+  qty: { type: Number, required: true, min: 1 },
 });
 
-export default mongoose.model("Orders", orderSchema, "Orders");
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  company: { type: String, required: true },
+  email: { type: String, required: true },
+});
+
+const OrderSchema = new mongoose.Schema(
+  {
+    user: { type: UserSchema, required: true },
+    products: { type: [ProductSchema], required: true },
+    total: {type: Number, required: true}
+  },
+  { timestamps: true }
+);
+
+
+export default mongoose.model("Order", OrderSchema, "Orders");
